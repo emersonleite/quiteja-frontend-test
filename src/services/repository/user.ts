5e1b5d0a750692ca534ec4a764d16104d2b898f7
Emerson Leite <emerson.bl@gmail.com>
2024-11-30 19:52:16 -0300

    chore: create getUserBy and removeUser services

--- src/services/repository/user.ts
+++ src/services/repository/user.ts
@@ -1,5 +1,5 @@
 import { AxiosInstance } from "axios";
-import { UserData } from "@/types/user";
+import { UserByIdResponse, UserData } from "@/types/user";
 
 type RequestParams = { page: number; limit: number };
 
@@ -7,5 +7,14 @@ export default ($api: AxiosInstance) => ({
   async getAllUsers(params: RequestParams): Promise<UserData> {
     const response = await $api.get<UserData>("/user", { params });
     return response.data;
+  },
+
+  async getUserById(id: string): Promise<UserByIdResponse> {
+    const response = await $api.get<UserByIdResponse>(`/user/${id}`);
+    return response.data;
+  },
+
+  async removeUser(id: string): Promise<void> {
+    await $api.delete(`/user/${id}`);
   },
 });
