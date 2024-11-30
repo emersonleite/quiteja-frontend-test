e31a643f614f8f992aa5c65a46c7fdd797f7e247
Emerson Leite <emerson.bl@gmail.com>
2024-11-30 19:42:52 -0300

    chore: create mainTheme at vuetify plugin

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,10 +1,24 @@
-// Styles
-import '@mdi/font/css/materialdesignicons.css'
-import 'vuetify/styles'
+import "@mdi/font/css/materialdesignicons.css";
+import "vuetify/styles";
 
-// Vuetify
-import { createVuetify } from 'vuetify'
+import { createVuetify } from "vuetify";
 
-export default createVuetify(
-  // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
-)
+export default createVuetify({
+  theme: {
+    themes: {
+      mainTheme: {
+        dark: false,
+        colors: {
+          background: "#f3f3f3",
+          surface: "#ffffff",
+          primary: "#1B5F6B",
+          secondary: "#49E388",
+          error: "#f26f66",
+          info: "#3b82f6",
+          success: "#22c55e",
+          warning: "#ffc943",
+        },
+      },
+    },
+  },
+});
