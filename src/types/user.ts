f919f10ab541b9f20568f1f5c6a8553f54d8deb9
Emerson Leite <emerson.bl@gmail.com>
2024-11-30 19:51:39 -0300

    chore: create UserAddress and UserByIdResponse types

--- src/types/user.ts
+++ src/types/user.ts
@@ -12,3 +12,26 @@ export type UserData = {
   page: number;
   limit: number;
 };
+
+type UserAddress = {
+  street: string;
+  city: string;
+  state: string;
+  country: string;
+  timezone: string;
+};
+
+export type UserByIdResponse = {
+  id: string;
+  title: string;
+  firstName: string;
+  lastName: string;
+  picture: string;
+  gender: "male" | "female";
+  email: string;
+  dateOfBirth: string;
+  phone: string;
+  location: UserAddress;
+  registerDate: string;
+  updatedDate: string;
+};
