e995a0804d3b51efe3f8db459afe71ff62cca7a7
Emerson Leite <emerson.bl@gmail.com>
2024-11-30 13:54:13 -0300

    chore: update to vue 3 (typescript support), add pinia, add vuetify, add axios

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+  <v-app>
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script lang="ts">
+import { defineComponent } from 'vue'
 
-nav {
-  padding: 30px;
+export default defineComponent({
+  name: 'App',
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+  data () {
+    return {
+      //
     }
-  }
-}
-</style>
+  },
+})
+</script>
