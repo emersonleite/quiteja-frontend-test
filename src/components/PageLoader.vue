7ee54d86ab707fc7f769d06e4161d26fab15f995
Emerson Leite <emerson.bl@gmail.com>
2024-12-01 09:09:02 -0300

    chore: create PageLoader compoent

---
+++ src/components/PageLoader.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-skeleton-loader
+    v-if="props.loading"
+    :height="props.height"
+    :type="props.type"
+    :max-width="props.maxWidth && props.maxWidth"
+  ></v-skeleton-loader>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from "vue";
+
+const props = defineProps({
+  loading: {
+    type: Boolean,
+    required: true,
+  },
+  height: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  maxWidth: {
+    type: String,
+    required: false,
+  },
+});
+</script>
