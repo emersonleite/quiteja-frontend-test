db77b6727a101d5bd51998b1c16a46c538ea840d
Emerson Leite <emerson.bl@gmail.com>
2024-12-01 11:36:48 -0300

    chore: add piniaPluginPersistedstate to main

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 import App from "./App.vue";
 import router from "./router";
 import vuetify from "./plugins/vuetify";
 import { loadFonts } from "./plugins/webfontloader";
 
 const pinia = createPinia();
+pinia.use(piniaPluginPersistedstate);
 
 loadFonts();
 
