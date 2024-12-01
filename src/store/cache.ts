9df7e1a9df7100808f1436f6b145b0a04f2adf70
Emerson Leite <emerson.bl@gmail.com>
2024-12-01 11:35:13 -0300

    chore: add totalItems to store

--- src/store/cache.ts
+++ src/store/cache.ts
@@ -2,31 +2,37 @@ import { defineStore } from "pinia";
 import { ref } from "vue";
 import { User } from "@/types/user";
 
-export const useCacheStore = defineStore("cache", () => {
-  const time = ref<number>(0);
+export const useCacheStore = defineStore(
+  "cache",
+  () => {
+    const time = ref<number>(0);
 
-  const users = ref<User[]>([]);
+    const users = ref<User[]>([]);
 
-  const totalItems = ref<number>(1);
+    const totalItems = ref<number>(1);
 
-  function setTime(newTime: number) {
-    time.value = newTime;
+    function setTime(newTime: number) {
+      time.value = newTime;
+    }
+
+    function setUsers(usersList: User[]) {
+      users.value = usersList;
+    }
+
+    function setTotalItems(total: number) {
+      totalItems.value = total;
+    }
+
+    return {
+      time,
+      users,
+      totalItems,
+      setTime,
+      setUsers,
+      setTotalItems,
+    };
+  },
+  {
+    persist: true,
   }
-
-  function setUsers(usersList: User[]) {
-    users.value = usersList;
-  }
-
-  function setTotalItems(total: number) {
-    totalItems.value = total;
-  }
-
-  return {
-    time,
-    users,
-    totalItems,
-    setTime,
-    setUsers,
-    setTotalItems,
-  };
-});
+);
